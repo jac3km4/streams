fa5a056ce920b8e75d4e4aff7d90951fcb562cc9
Jacek Malec <jac3km4@gmail.com>
2016-04-02 00:02:20 +0200

    improve doc and a couple of fixes for memory stream

--- source/streams/memory.d
+++ source/streams/memory.d
@@ -17,22 +17,22 @@ private {
 }
 
 /**
- * Creates a read-only memory stream from an array.
+ * Creates a read-only memory stream from a byte array.
  * 
  * Params:
  * 	data = Immutable byte array.
  */
-static @nogc auto memoryStream(immutable(ubyte)[] data) nothrow {
+static @nogc @safe auto memoryStream(immutable(ubyte)[] data) pure nothrow {
 	return ReadOnlyMemoryStream(data);
 }
 
 /**
- * Creates a memory stream based on an array.
+ * Creates a memory stream based on a byte array.
  * 
  * Params:
  * 	data = Byte array.
  */
-static auto memoryStream(ubyte[] data) nothrow {
+static auto @safe memoryStream(ubyte[] data) pure nothrow {
 	return MemoryStream(data);
 }
 
@@ -42,14 +42,14 @@ static auto memoryStream(ubyte[] data) nothrow {
  * Params:
  * 	size = Amount of bytes to preallocate.
  */
-static auto memoryStream(size_t size) nothrow {
+static auto @safe memoryStream(size_t size) pure nothrow {
 	return MemoryStream(size);
 }
 
 /**
  * Creates a memory stream.
  */
-static @nogc auto memoryStream() nothrow {
+static @nogc @safe auto memoryStream() pure nothrow {
 	return MemoryStream();
 }
 
@@ -84,10 +84,20 @@ struct ReadOnlyMemoryStream {
 	private size_t position = 0;
 	private immutable (ubyte)[] buffer;
 
+	/**
+	 * Creates a read-only memory stream from a byte array.
+	 * 
+	 * Params:
+	 * 	data = Immutable byte array.
+	 */
 	@nogc @safe this(immutable(ubyte)[] data) pure nothrow {
 		buffer = data;
 	}
 
+	/**
+	 * Reads bytes into the specified buffer.
+	 * The number of bytes read is returned.
+	 */
 	@nogc @safe size_t read(ubyte[] buf) pure nothrow {
 		auto remaining = buffer.length - position;
 		size_t len;
@@ -101,6 +111,11 @@ struct ReadOnlyMemoryStream {
 		return len;
 	}
 
+	/**
+	 * Provides a direct access to a number of bytes.
+	 * If remaining bytes is less than size, then a
+	 * smaller slice is returned.
+	 */
 	@nogc @safe const(ubyte[]) read(size_t size) pure nothrow {
 		auto remaining = buffer.length - position;
 		size_t len;
@@ -114,6 +129,13 @@ struct ReadOnlyMemoryStream {
 		return slice;
 	}
 
+	/**
+	 * Seeks relative to a position.
+	 *
+	 * Params:
+	 *   offset = Offset relative to a reference point.
+	 *   from   = Optional reference point.
+	 */
 	long seekTo(long offset, From from = From.start) {
 		switch(from) {
 			case From.start:
@@ -134,10 +156,16 @@ struct ReadOnlyMemoryStream {
 		return position;
 	}
 
+	/**
+	 * Returns the underlying buffer.
+	 */
 	@nogc @safe @property immutable(ubyte[]) data() pure nothrow {
 		return buffer;
 	}
 
+	/**
+	 * Returns a slice of the underlying buffer.
+	 */
 	@nogc @safe @property immutable(ubyte[]) opSlice(size_t i, size_t j) pure nothrow {
 		return buffer[i..j];
 	}
@@ -150,14 +178,30 @@ struct MemoryStream {
 	private size_t position = 0;
 	private Appender!(ubyte[]) buffer;
 
+	/**
+	 * Creates a memory stream based on a byte array.
+	 * 
+	 * Params:
+	 * 	data = Byte array.
+	 */
 	@safe this(ubyte[] data) pure nothrow {
 		buffer = appender(data);
 	}
 
+	/**
+	 * Creates a memory stream reserving specified amount of memory.
+	 * 
+	 * Params:
+	 * 	size = Amount of bytes to preallocate.
+	 */
 	@safe this(size_t size) pure nothrow {
 		buffer.reserve(size);
 	}
 
+	/**
+	 * Reads bytes into the specified buffer.
+	 * The number of bytes read is returned.
+	 */
 	@nogc @safe size_t read(ubyte[] buf) pure nothrow {
 		auto data = buffer.data;
 		auto remaining = data.length - position;
@@ -172,6 +216,11 @@ struct MemoryStream {
 		return len;
 	}
 
+	/**
+	 * Provides a direct access to a number of bytes.
+	 * If remaining bytes is less than size, then a
+	 * smaller slice is returned.
+	 */
 	@nogc @safe const(ubyte[]) read(size_t size) pure nothrow {
 		auto data = buffer.data;
 		auto remaining = data.length - position;
@@ -184,13 +233,29 @@ struct MemoryStream {
 		auto slice = data[position..position+len];
 		position += len;
 		return slice;
-	}	
+	}
+
+	/**
+	 * Appends data to the buffer.
+	 *
+	 * Params:
+	 *   data = Byte array to be appended.
+	 *
+	 * Returns: The number of bytes that were appended.
+	 */
 	@safe size_t write(in ubyte[] data) pure nothrow {
 		buffer ~= data;
 		position += data.length;
 		return data.length;
 	}
 
+	/**
+	 * Seeks relative to a position.
+	 *
+	 * Params:
+	 *   offset = Offset relative to a reference point.
+	 *   from   = Optional reference point.
+	 */
 	long seekTo(long offset, From from = From.start) {
 		auto data = buffer.data;
 		switch(from) {
@@ -212,10 +277,16 @@ struct MemoryStream {
 		return position;
 	}
 
-	@nogc @safe @property const(ubyte)[] data() nothrow pure {
+	/**
+	 * Returns the underlying buffer.
+	 */
+	@nogc @safe @property const(ubyte)[] data() pure nothrow {
 		return buffer.data;
 	}
 
+	/**
+	 * Returns a slice of the underlying buffer.
+	 */
 	@nogc @safe @property const(ubyte[]) opSlice(size_t i, size_t j) pure nothrow {
 		return buffer.data[i..j];
 	}
